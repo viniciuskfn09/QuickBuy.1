33baad0defbd205bc01875dbba32937c50b4ca7f
viniciuskfn09 <58142271+viniciuskfn09@users.noreply.github.com>
2020-03-11 11:31:40 -0300

    Cricacao das entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using QuickBuy.Dominio.Entidades.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NomeEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+        //Pedido deve ter pelo menos um item de pedido ou varios itens de pedidos.
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        //Um usuario pode ter um ou varios pedidos.
+
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
