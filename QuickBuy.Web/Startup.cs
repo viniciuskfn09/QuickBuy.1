1b2530b8f13b79d59e40227e2df30135bce7421c
viniciuskfn09 <58142271+viniciuskfn09@users.noreply.github.com>
2020-03-12 11:58:22 -0300

    atualizaçoes

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuickBuy.Repositorio.Contexto;
 
 namespace QuickBuy.Web
 {
@@ -12,8 +14,9 @@ namespace QuickBuy.Web
     {
         public Startup(IConfiguration configuration)
         {
-            var builder = new configuration
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("config.json",optional: false, reloadOnChange:true);
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -21,6 +24,10 @@ namespace QuickBuy.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("MySqlConnection");
+            services.AddDbContext<QuickBuyContexto>(option =>
+                                     option.UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
